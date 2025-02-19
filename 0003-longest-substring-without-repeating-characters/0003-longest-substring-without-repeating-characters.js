0918a28ed48bd955c8b77242f75ee8d87e542105
Enrico Freitas <91137476+Enr1coFreitas@users.noreply.github.com>
2025-02-19 16:05:16 -0300

    Time: 3 ms (98.81%), Space: 59 MB (6.1%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let set = new Set();
+    let left = 0, maxLength = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        while (set.has(s[right])) {
+            set.delete(s[left]);
+            left++;
+        }
+        
+        set.add(s[right]);
+        maxLength = maxLength < (right - left + 1) ? (right - left + 1) : maxLength;
+    }
+
+    return maxLength;
+};
